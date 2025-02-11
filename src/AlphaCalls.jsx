ec4388eb57f21a2d6ce8d84c667e947b4f9b1629
bigmacnomeat <baleytaylor9898@gmail.com>
2025-02-11 00:04:19 -0500

    Create AlphaCalls.jsx

---
+++ src/AlphaCalls.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AlphaCalls = () => {
+    return (
+        <div>
+            <h2>Alpha Calls Component</h2>
+            {/* Add your component logic here */}
+        </div>
+    );
+};
+
+export default AlphaCalls;
